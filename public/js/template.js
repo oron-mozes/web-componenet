2c0593dad9be60a3d061ac2eeae15e35d9851bcc
mozesoron <oronm@wix.com>
2020-08-17 16:08:14 +0300

    fix set

--- public/js/template.js
+++ public/js/template.js
@@ -1,11 +1,10 @@
 async function getData() {
     const result = await fetch('https://oron-web-component-wrapper.herokuapp.com/data');
     const json = await result.json();
-    document.getElementsByTagName('my-paragraph').innerHTML = json.data.allow ? 'GET pass' : 'FAIL TO GET!!!!';
+    document.getElementsByTagName('my-paragraph')[0].innerHTML = json.data.allow ? 'GET pass' : 'FAIL TO GET!!!!';
     console.info( json.data);
 }
 async function postData() {
-    debugger;
     const result = await fetch('https://oron-web-component-wrapper.herokuapp.com/data', { method: 'POST'});
     const json = await result.json();
     console.error( json.data);
