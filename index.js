4b75b208eb01307d43920471b745fc3c71681e21
mozesoron <oronm@wix.com>
2020-08-17 10:38:17 +0300

    add cors

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
 
 express()
+    .use(cors({
+        "origin": "*",
+        "methods": "GET",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
